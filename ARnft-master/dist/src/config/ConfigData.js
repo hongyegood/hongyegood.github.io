c893b0b1e0c4f718644b79c51dd4b8ec27de429a
hongyegood <401774187@qq.com>
2022-07-23 14:27:13 +0800

    aa

--- ARnft-master/dist/src/config/ConfigData.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=ConfigData.js.map
--- ARnft-master/types/src/utils/ARnftUtils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function degreesToRadians(degrees: number): number;
-export declare function isMobile(): boolean;
-export declare function isIOS(): boolean;
-export declare function getWindowSize(vw: number, vh: number): Array<number>;
-export declare function getConfig(configData: string): Promise<any>;
